b98c91307df9777bcfa3538fbae878f2a86e0211
HyeranShin <hyeran9712@naver.com>
2019-08-04 00:59:45 +0900

    한글 이름 추천 데이터 삽입

---
+++ app/src/main/java/com/example/daon/Name.kt
@@ -0,0 +1,8 @@
+package com.example.daon
+
+class Name constructor(name: String, pronunciation: String, meaningKor: String, meaningEng: String) {
+    var name: String = name
+    var pronunciation: String = pronunciation
+    var meaningKor: String = meaningKor
+    var meaningEng: String = meaningEng
+}
