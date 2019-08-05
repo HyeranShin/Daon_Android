e39212cf40f25c03d26e00e5c1d1731f8a9851b1
HyeranShin <hyeran9712@naver.com>
2019-08-05 16:01:38 +0900

    InfoEnterActivity UI 구성

--- app/src/main/java/com/example/daon/InfoEnterActivity.kt
+++ app/src/main/java/com/example/daon/InfoEnterActivity.kt
@@ -1,7 +1,10 @@
 package com.example.daon
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_info_enter.*
 import org.jetbrains.anko.startActivity
 
@@ -18,5 +21,18 @@ class InfoEnterActivity : AppCompatActivity() {
         btn_next_info_enter.setOnClickListener {
             startActivity<NameRcmdActivity>()
         }
+        tv_male_info_enter.setOnClickListener {
+            changeViewColor(tv_male_info_enter, tv_female_info_enter)
+        }
+        tv_female_info_enter.setOnClickListener {
+            changeViewColor(tv_female_info_enter, tv_male_info_enter)
+        }
+    }
+
+    private fun changeViewColor(selected: TextView, notSelected: TextView) {
+        selected.setBackgroundResource(R.drawable.round_20_main_color)
+        selected.setTextColor(Color.WHITE)
+        notSelected.background = null
+        notSelected.setTextColor(resources.getColor(R.color.mainColor))
     }
 }
