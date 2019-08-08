a7fc666ba6cbdc7bc7217a6f049fdf3ba0d88a02
HyeranShin <hyeran9712@naver.com>
2019-08-08 14:44:35 +0900

    PickUpLocActivity UI 구성

--- app/src/main/java/com/example/daon/AfterOrderActivity.kt
+++ app/src/main/java/com/example/daon/AfterOrderActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.daon.controller.SharedPreferenceController
 import kotlinx.android.synthetic.main.activity_after_order.*
+import org.jetbrains.anko.startActivity
 
 class AfterOrderActivity : AppCompatActivity() {
 
@@ -11,8 +12,16 @@ class AfterOrderActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_after_order)
 
+        setOnClickListener()
+
         tv_big_guide_after_order.text = "${SharedPreferenceController.getName(applicationContext)}님의 \n명함을 \n제작하고 있어요."
         //TODO 언어에 따라
 //        tv_big_guide_after_order.text = "We are making \n${SharedPreferenceController.getName(applicationContext)}\'s \nname card."
+    }
+
+    private fun setOnClickListener() {
+        btn_map_after_order.setOnClickListener {
+            startActivity<PickUpLocActivity>()
+        }
     }
 }
---
+++ app/src/main/java/com/example/daon/PickUpLocActivity.kt
@@ -0,0 +1,12 @@
+package com.example.daon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PickUpLocActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pick_up_loc)
+    }
+}
